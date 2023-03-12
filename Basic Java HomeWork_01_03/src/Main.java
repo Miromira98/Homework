5461f650507721d1516c495ca53af8206763f564
Miromira98 <125549247+Miromira98@users.noreply.github.com>
2023-03-12 18:42:06 +0100

    Add files via upload

---
+++ Basic Java HomeWork_01_03/src/Main.java
@@ -0,0 +1,17 @@
+import java.security.SecureRandom;
+
+public class Main {
+    public static void main(String[] args) {
+           boolean tomorrowItWillRain = true;
+           int number = 40;
+           String nameOfTheStreet = "Lenina";
+           double number1 = 25.5;
+
+
+            System.out.println(tomorrowItWillRain);
+            System.out.println(number);
+            System.out.println(nameOfTheStreet);
+            System.out.println(number1);
+    }
+
+}
---
+++ Basic Java HomeWork_01_03/src/Main1.java
@@ -0,0 +1,48 @@
+public class Main1 {
+    public static void main(String[] args) {
+        add();
+        subtraction();
+        multiplication();
+        division();
+
+    }
+
+    public static void add() {
+        int number0 = 70;
+        int number1 = 36;
+
+        int resultOfAddition = number0 + number1;
+
+        System.out.println(resultOfAddition);
+    }
+
+    public static void subtraction() {
+        int number0 = 80;
+        int number1 = 25;
+
+        int resultOfSubtraction = number0 - number1;
+
+        System.out.println(resultOfSubtraction);
+
+    }
+
+    public static void multiplication() {
+        int number0 = 240;
+        int number1 = 7;
+
+        int resultOfMultiplication = number0 * number1;
+
+        System.out.println(resultOfMultiplication);
+
+    }
+
+    public static void division() {
+        int number0 = 8;
+        int number1 = 2560;
+
+        int resultOfDivision = number1 / number0;
+
+        System.out.println(resultOfDivision);
+
+    }
+}
